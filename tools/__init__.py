74213ea21dc7be816007aed99f4d64e096c95ef8
Samuele Marro <marrosamuele@mail.com>
2024-08-26 19:38:24 +0000

    Added call_tool.py

--- tools/__init__.py
+++ tools/__init__.py
@@ -1,3 +1,11 @@
 from .dummy import weather_tool, rain_tool
 
 TOOLS = [weather_tool, rain_tool]
+
+def call_tool(tool_name, **arguments):
+    tool = next((tool for tool in TOOLS if tool.name == tool_name), None)
+
+    if tool is not None:
+        return tool.function(**arguments)
+    else:
+        return 'Tool not found'

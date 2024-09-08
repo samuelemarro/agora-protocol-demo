06cb386315689d68e50a2e075b6713aaa594a031
Samuele Marro <marrosamuele@mail.com>
2024-09-08 16:06:04 +0000

    Added memory & metadata support to protocol DB.

--- agents/protocol_db/main.py
+++ agents/protocol_db/main.py
@@ -4,6 +4,12 @@ sys.path.append('.')
 import dotenv
 dotenv.load_dotenv()
 
+import os
+from pathlib import Path
+
+if os.environ.get('STORAGE_PATH') is None:
+    os.environ['STORAGE_PATH'] = str(Path().parent / 'storage' / 'protocol_db')
+
 from flask import Flask, request
 
 import json
@@ -18,7 +24,14 @@ PROTOCOLS = {}
 def main():
     return json.dumps({
         'status': 'success',
-        'protocols' : list(PROTOCOLS.keys())
+        'protocols' : [
+            {
+                'id': protocol_id,
+                'name': protocol_data['name'],
+                'description': protocol_data['description'],
+            }
+            for protocol_id, protocol_data in PROTOCOLS.items()
+        ]
     })
 
 @app.route('/', methods=['POST'])
@@ -32,13 +45,12 @@ def add_protocol():
         })
 
     protocol = data['protocol']
-
-
     hashed_protocol = compute_hash(protocol)
 
-    PROTOCOLS[hashed_protocol] = protocol
+    PROTOCOLS[hashed_protocol] = data
 
     print('Added protocol:', hashed_protocol)
+    save_memory()
 
     return json.dumps({
         'status': 'success'
@@ -48,6 +60,10 @@ def add_protocol():
 @app.route('/protocol', methods=['GET'])
 def get_protocol():
     protocol_id = request.args.get('id')
+
+    print('Received request for protocol:', protocol_id)
+    print('Current keys:', PROTOCOLS.keys())
+    print('Do I have the protocol?', protocol_id in PROTOCOLS)
     if protocol_id not in PROTOCOLS:
         return json.dumps({
             'status': 'error',
@@ -55,4 +71,60 @@ def get_protocol():
         })
     
     # No JSON
-    return PROTOCOLS[protocol_id]
+    return PROTOCOLS[protocol_id]['protocol']
+
+@app.route('/metadata', methods=['GET'])
+def get_metadata():
+    protocol_id = request.args.get('id')
+
+    print('Received request for metadata:', protocol_id)
+    print('Current keys:', PROTOCOLS.keys())
+    print('Do I have the protocol?', protocol_id in PROTOCOLS)
+    if protocol_id not in PROTOCOLS:
+        return json.dumps({
+            'status': 'error',
+            'message': 'Protocol not found.'
+        })
+    
+    return json.dumps({
+        'status': 'success',
+        'metadata': {
+            'name': PROTOCOLS[protocol_id]['name'],
+            'description': PROTOCOLS[protocol_id]['description']
+        }
+    })
+
+def load_memory():
+    PROTOCOLS.clear()
+
+    storage_path = Path(os.environ.get('STORAGE_PATH'))
+
+    if not storage_path.exists():
+        print('No storage path found, using empty memory')
+        return
+    
+    memory_file = storage_path / 'memory.json'
+
+    with open(memory_file, 'r') as f:
+        memory = json.load(f)
+    for protocol_id, protocol_data in memory.items():
+        PROTOCOLS[protocol_id] = protocol_data
+
+    print('Loaded memory:', PROTOCOLS)
+
+def save_memory():
+    storage_path = Path(os.environ.get('STORAGE_PATH'))
+
+    if not storage_path.exists():
+        storage_path.mkdir(parents=True)
+
+    memory_file = storage_path / 'memory.json'
+
+    with open(memory_file, 'w') as f:
+        json.dump(PROTOCOLS, f)
+
+def init():
+    load_memory()
+
+
+init()
--- orchestrator.py
+++ orchestrator.py
@@ -61,6 +61,7 @@ def main():
         session = tmux_server.new_session(session_name=protocol_db_id, kill_session=True)
         pane = session.active_window.active_pane
         port = id_to_url_mappings[protocol_db_id].split(':')[-1]
+        storage_path = base_storage_path / 'protocol_db' / protocol_db_id
         log_path = base_log_path / 'protocol_db' / (protocol_db_id + '.log')
         log_path.parent.mkdir(parents=True, exist_ok=True)
         pane.send_keys(f'PYTHONUNBUFFERED=1 flask --app agents/protocol_db/main.py run --port {port} 2>&1 | tee {log_path}')
